9128f43a321dafb57249806f91db6eccf36dd57b
David Svara <dsvara@outlook.cz>
2025-04-16 17:18:48 +0200

    adding all

--- lidovky.py
+++ lidovky.py
@@ -8,37 +8,21 @@ import webbrowser
 from urllib.parse import quote_plus
 from pathlib import Path
 
-## === important for edge brw === ##
-#import subprocess
-#import os
-
 # ==== NASTAVENÍ ====
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0"
 }
 COOKIES_FILE = "cookies.json"
-NAVSTIVENE_SOUBOR = "navstivene.txt"
+NAVSTIVENE_SOUBOR = "navstivene_lidovky.txt"
 SOUTEZNI_REGEX = re.compile(r"https://www\.idnes\.cz/ekonomika/megahra-o-auto[^\"]+")
 
 # ==== FUNKCE ====
-#def otevri_edge_a_zavri(url, cekani_vterin=8):
-    #try:
-        ## Spustí Microsoft Edge s daným URL
-        #subprocess.Popen(['start', 'msedge', url], shell=True)
-        #log_udalost(f"Otevřen Edge s URL: {url}")
-        #time.sleep(cekani_vterin)
-
-        # Zavře celý Edge (pozor: i ostatní okna/taby!)
-        #os.system("taskkill /IM msedge.exe /F")
-        #log_udalost("Edge zavřen.")
-    #except Exception as e:
-        #log_udalost(f"Chyba při otevírání/zavírání Edge: {e}")
-
 def load_cookies(filename):
     import json
     with open(filename, "r", encoding="utf-8") as f:
         raw = json.load(f)
     return {cookie["name"]: cookie["value"] for cookie in raw}
+
 
 def log_udalost(text):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -58,21 +42,20 @@ def uloz_navstiveny(odkaz):
     with open(NAVSTIVENE_SOUBOR, "a", encoding="utf-8") as f:
         f.write(odkaz + "\n")
 
+
 def je_prihlaseny(cookies):
     try:
         r = requests.get("https://www.idnes.cz/ucet", cookies=cookies, headers=HEADERS)
         return "David Švára" in r.text
     except:
         return False
+    
+
 
 def ziskej_odkazy_z_archivu(datum, stranka=1):
-    url = "https://www.idnes.cz/zpravy/archiv"
-    if stranka > 1:
-        url = f"{url}/{stranka}"
-    params = {
-        "datum": datum,
-        "idostrova": "idnes"
-    }
+    #url = "https://www.lidovky.cz/orientace"
+    url = f"https://www.lidovky.cz/data.aspx?type=infinitesph&r=sph&section=lidovky&strana={stranka}&version=sph2024"
+
     r = requests.get(url, cookies=cookies, headers=HEADERS) #, params=params)
     print(f"Ziskej odkazy z archivu: {url}")  # a parametry {params}")
     soup = BeautifulSoup(r.text, "html.parser")
@@ -87,10 +70,12 @@ def ziskej_odkazy_z_archivu(datum, stranka=1):
     #odkazy = [a["href"] for a in soup.select("a[href^='https://www.idnes.cz/']") if "/zpravy/" in a["href"]]
     return list(set(odkazy))  # odstraní duplicity
 
+
 # ==== HLAVNÍ LOGIKA ====
 
 datum_puvodni = "1. 1. 2025"  # zapsáno s mezerami
 datum = quote_plus(datum_puvodni)  # výstup bude "1.+1.+2025"
+
 
 cookies = load_cookies(COOKIES_FILE)
 if not je_prihlaseny(cookies):
